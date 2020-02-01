ee6327710123c6722c725e6ec09fe75140328f13
Dani Matuko <matuko305@gmail.com>
2020-02-02 00:05:19 +0200

    hamburger animation

--- javascript/app.js
+++ javascript/app.js
@@ -4,5 +4,5 @@ const menu = document.querySelector('#main-nav');
 /* Event Listenters */
 btnHamburger.addEventListener('click', function() {
     menu.classList.toggle('open');
-    btnHamburger.classList.toggle('sticky');
+    btnHamburger.classList.toggle('pressed');
 })
