650f6fbcff401a9811776e1122d3ec9f5b93543d
Dani Matuko <matuko305@gmail.com>
2020-02-01 23:50:01 +0200

    mobieMenu

--- javascript/app.js
+++ javascript/app.js
@@ -4,4 +4,5 @@ const menu = document.querySelector('#main-nav');
 /* Event Listenters */
 btnHamburger.addEventListener('click', function() {
     menu.classList.toggle('open');
+    btnHamburger.classList.toggle('sticky');
 })
